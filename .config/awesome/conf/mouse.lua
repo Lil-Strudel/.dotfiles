792553c20a09002bbfc0edf943edf30a5a36cbf2
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-05 13:33:02 -0700

    test

--- .config/awesome/conf/mouse.lua
+++ .config/awesome/conf/mouse.lua
@@ -13,9 +13,9 @@ awful.mouse.append_global_mousebindings {
 
 client.connect_signal("request::default_mousebindings", function()
     awful.mouse.append_client_mousebindings {
-        -- awful.button({ }, 1, function (c)
-        --     c:emit_signal("request::activate", "mouse_click", {raise = true})
-        -- end),
+        awful.button({ }, 1, function (c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+        end),
         -- awful.button({ modkey }, 1, function (c)
         --     c:emit_signal("request::activate", "mouse_click", {raise = true})
         --     awful.mouse.client.move(c)
