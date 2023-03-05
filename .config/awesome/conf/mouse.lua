424f284042648e2b36045b37b6f51ed4c85b55f1
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-05 12:52:03 -0700

    feat: client keybinds + mouse events

--- .config/awesome/conf/mouse.lua
+++ .config/awesome/conf/mouse.lua
@@ -1,8 +1,28 @@
 local awful = require("awful")
-local gears = require("gears")
 
-root.buttons(gears.table.join(
-    awful.button({ }, 3, function () M.main:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
+awful.mouse.append_global_mousebindings {
+    awful.button({}, 3, function()
+        M.main:toggle()
+    end),
+
+    awful.button({}, 4, awful.tag.viewprev),
+
+    awful.button({}, 5, awful.tag.viewnext),
+}
+
+
+client.connect_signal("request::default_mousebindings", function()
+    awful.mouse.append_client_mousebindings {
+        awful.button({ }, 1, function (c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+        end),
+        awful.button({ modkey }, 1, function (c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+            awful.mouse.client.move(c)
+        end),
+        awful.button({ modkey }, 3, function (c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+            awful.mouse.client.resize(c)
+        end)
+    }
+end)
--- .config/awesome/conf/rules.lua
+++ .config/awesome/conf/rules.lua
@@ -4,14 +4,13 @@ local beautiful = require("beautiful")
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = ClientKeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
+      properties = { 
+        border_width = beautiful.border_width,
+        border_color = beautiful.border_normal,
+        focus = awful.client.focus.filter,
+        raise = true,
+        screen = awful.screen.preferred,
+        placement = awful.placement.no_overlap+awful.placement.no_offscreen
      }
     },
 
