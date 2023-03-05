013e6901fc3d5879a11600f148bc77b3050a56fc
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-05 13:22:04 -0700

    test

--- .config/awesome/conf/mouse.lua
+++ .config/awesome/conf/mouse.lua
@@ -11,18 +11,18 @@ awful.mouse.append_global_mousebindings {
 }
 
 
--- client.connect_signal("request::default_mousebindings", function()
---     awful.mouse.append_client_mousebindings {
---         awful.button({ }, 1, function (c)
---             c:emit_signal("request::activate", "mouse_click", {raise = true})
---         end),
---         awful.button({ modkey }, 1, function (c)
---             c:emit_signal("request::activate", "mouse_click", {raise = true})
---             awful.mouse.client.move(c)
---         end),
---         awful.button({ modkey }, 3, function (c)
---             c:emit_signal("request::activate", "mouse_click", {raise = true})
---             awful.mouse.client.resize(c)
---         end)
---     }
--- end)
+client.connect_signal("request::default_mousebindings", function()
+    awful.mouse.append_client_mousebindings {
+        awful.button({ }, 1, function (c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+        end),
+        awful.button({ modkey }, 1, function (c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+            awful.mouse.client.move(c)
+        end),
+        awful.button({ modkey }, 3, function (c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+            awful.mouse.client.resize(c)
+        end)
+    }
+end)
