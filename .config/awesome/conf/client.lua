e7172e2ff65139a099ab25eccc45a86b71cf58e4
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-05 11:45:27 -0700

    fix: added wibox to client

--- .config/awesome/conf/client.lua
+++ .config/awesome/conf/client.lua
@@ -1,6 +1,7 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 local gears = require("gears")
+local wibox = require("wibox")
 
 client.connect_signal("manage", function (c)
   -- Set the windows at the slave,
