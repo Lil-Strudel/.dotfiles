6812de065a99387eb17f83f62c0ce66d5416b9e2
Lil Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-05 19:05:44 -0700

    move notifications ot file

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -24,24 +24,4 @@ _G.theme = "tokyo-night"
 beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/" .. theme .. "/theme.lua")
 -- }}}
 
--- {{{ Notifications
-
-ruled.notification.connect_signal('request::rules', function()
-    -- All notifications will match this rule.
-    ruled.notification.append_rule {
-        rule       = { },
-        properties = {
-            screen           = awful.screen.preferred,
-            implicit_timeout = 5,
-        }
-    }
-end)
-
-naughty.connect_signal("request::display", function(n)
-    naughty.layout.box { notification = n }
-end)
-
--- }}}
-
-
 require "config"
