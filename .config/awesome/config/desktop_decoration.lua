b9156f8605f0057c0bb376a9aea707b342194973
Lil Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-05 21:01:27 -0700

    ,

--- .config/awesome/config/desktop_decoration.lua
+++ .config/awesome/config/desktop_decoration.lua
@@ -68,7 +68,7 @@ screen.connect_signal("request::desktop_decoration", function(s)
     s.mywibox = awful.wibar {
         position = "top",
         screen   = s,
-        margins  = dpi(5)
+        margins  = dpi(5),
         widget   = {
             layout = wibox.layout.align.horizontal,
             { -- Left widgets
