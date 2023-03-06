0b39d2fa62f1ad4ef270ee7f4da57356c220b6ae
Lil Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-05 19:35:51 -0700

    refactoring desktop_decoration

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,27 +1,14 @@
 -- awesome_mode: api-level=4:screen=on
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
--- Standard awesome library
 local gears = require("gears")
+local beautiful = require("beautiful")
 local awful = require("awful")
 require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
-local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
--- Declarative object management
-local ruled = require("ruled")
 
 require "config.error_handling"
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
 _G.theme = "tokyo-night"
 beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/" .. theme .. "/theme.lua")
--- }}}
 
 require "config"
