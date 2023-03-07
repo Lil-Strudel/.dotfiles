d751e4a87caeb74e80b60500cdaf96863fb923c4
Lil Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-06 22:32:38 -0700

    feat: unstretch

--- .config/awesome/config/desktop_decoration.lua
+++ .config/awesome/config/desktop_decoration.lua
@@ -22,10 +22,10 @@ screen.connect_signal("request::desktop_decoration", function(s)
     s.mylayoutbox = awful.widget.layoutbox {
         screen  = s,
         buttons = {
-            awful.button({ }, 1, function () awful.layout.inc( 1) end),
-            awful.button({ }, 3, function () awful.layout.inc(-1) end),
-            awful.button({ }, 4, function () awful.layout.inc(-1) end),
-            awful.button({ }, 5, function () awful.layout.inc( 1) end),
+            awful.button({}, 1, function() awful.layout.inc(1) end),
+            awful.button({}, 3, function() awful.layout.inc(-1) end),
+            awful.button({}, 4, function() awful.layout.inc(-1) end),
+            awful.button({}, 5, function() awful.layout.inc(1) end),
         }
     }
 
@@ -34,20 +34,20 @@ screen.connect_signal("request::desktop_decoration", function(s)
         screen  = s,
         filter  = awful.widget.taglist.filter.all,
         buttons = {
-            awful.button({ }, 1, function(t) t:view_only() end),
+            awful.button({}, 1, function(t) t:view_only() end),
             awful.button({ modkey }, 1, function(t)
-                                            if client.focus then
-                                                client.focus:move_to_tag(t)
-                                            end
-                                        end),
-            awful.button({ }, 3, awful.tag.viewtoggle),
+                if client.focus then
+                    client.focus:move_to_tag(t)
+                end
+            end),
+            awful.button({}, 3, awful.tag.viewtoggle),
             awful.button({ modkey }, 3, function(t)
-                                            if client.focus then
-                                                client.focus:toggle_tag(t)
-                                            end
-                                        end),
-            awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
-            awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end),
+                if client.focus then
+                    client.focus:toggle_tag(t)
+                end
+            end),
+            awful.button({}, 4, function(t) awful.tag.viewprev(t.screen) end),
+            awful.button({}, 5, function(t) awful.tag.viewnext(t.screen) end),
         }
     }
 
@@ -56,28 +56,30 @@ screen.connect_signal("request::desktop_decoration", function(s)
         screen  = s,
         filter  = awful.widget.tasklist.filter.currenttags,
         buttons = {
-            awful.button({ }, 1, function (c)
+            awful.button({}, 1, function(c)
                 c:activate { context = "tasklist", action = "toggle_minimization" }
             end),
-            awful.button({ }, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
-            awful.button({ }, 4, function() awful.client.focus.byidx(-1) end),
-            awful.button({ }, 5, function() awful.client.focus.byidx( 1) end),
+            awful.button({}, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
+            awful.button({}, 4, function() awful.client.focus.byidx(-1) end),
+            awful.button({}, 5, function() awful.client.focus.byidx(1) end),
         }
     }
 
     function round_wibox_shape(cr, width, height)
-	    gears.shape.rounded_rect(cr, width, height, 5)
+        gears.shape.rounded_rect(cr, width, height, 5)
     end
 
     -- Create the wiboxi
     s.left_wibox = awful.wibar {
+        stretch  = false,
         position = "top",
         screen   = s,
         shape    = round_wibox_shape,
         margins  = dpi(5),
         widget   = {
             layout = wibox.layout.align.horizontal,
-            { -- Left widgets
+            {
+                -- Left widgets
                 layout = wibox.layout.fixed.horizontal,
                 mylauncher,
                 s.mytaglist,
@@ -87,30 +89,33 @@ screen.connect_signal("request::desktop_decoration", function(s)
     }
 
     s.middle_wibox = awful.wibar {
-   	position = "top",
-	screen = s,
-	shape = round_wibox_shape,
-	margins = dpi(5),
-	widget = {
-		layout =wibox.layout.align.horizontal,
-		s.mytasklist,
-	},
+        stretch = false,
+        position = "top",
+        screen = s,
+        shape = round_wibox_shape,
+        margins = dpi(5),
+        widget = {
+            layout = wibox.layout.align.horizontal,
+            s.mytasklist,
+        },
     }
 
     s.right_wibox = awful.wibar {
-	    position = "top",
-	    screen = s,
-	    shape = round_wibox_shape,
-	    margins = dpi(5),
-	    widget = {
-		layout = wibox.layout.align.horizontal,
-		{ -- Right widgets
-		 layout = wibox.layout.fixed.horizontal,
-		 mykeyboardlayout,
-		 wibox.widget.systray(),
-		 mytextclock,
-		 s.mylayoutbox,
-		},
-	    },
+        stretch = false,
+        position = "top",
+        screen = s,
+        shape = round_wibox_shape,
+        margins = dpi(5),
+        widget = {
+            layout = wibox.layout.align.horizontal,
+            {
+                -- Right widgets
+                layout = wibox.layout.fixed.horizontal,
+                mykeyboardlayout,
+                wibox.widget.systray(),
+                mytextclock,
+                s.mylayoutbox,
+            },
+        },
     }
 end)
