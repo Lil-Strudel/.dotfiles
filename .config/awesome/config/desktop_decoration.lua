681d3591c9376f269d6fdd5420ab8dd1d3cbe59f
Lil Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2023-03-07 20:01:37 -0700

    feat: baddie tag list

--- .config/awesome/config/desktop_decoration.lua
+++ .config/awesome/config/desktop_decoration.lua
@@ -17,31 +17,31 @@ mytextclock = wibox.widget.textclock()
 
 
 local taglist_buttons = {
-            awful.button({}, 1, function(t) t:view_only() end),
-            awful.button({ modkey }, 1, function(t)
-                if client.focus then
-                    client.focus:move_to_tag(t)
-                end
-            end),
-            awful.button({}, 3, awful.tag.viewtoggle),
-            awful.button({ modkey }, 3, function(t)
-                if client.focus then
-                    client.focus:toggle_tag(t)
-                end
-            end),
-            awful.button({}, 4, function(t) awful.tag.viewprev(t.screen) end),
-            awful.button({}, 5, function(t) awful.tag.viewnext(t.screen) end),
-        }
+    awful.button({}, 1, function(t) t:view_only() end),
+    awful.button({ modkey }, 1, function(t)
+        if client.focus then
+            client.focus:move_to_tag(t)
+        end
+    end),
+    awful.button({}, 3, awful.tag.viewtoggle),
+    awful.button({ modkey }, 3, function(t)
+        if client.focus then
+            client.focus:toggle_tag(t)
+        end
+    end),
+    awful.button({}, 4, function(t) awful.tag.viewprev(t.screen) end),
+    awful.button({}, 5, function(t) awful.tag.viewnext(t.screen) end),
+}
 
 
 local function update_tag(item, tag, index)
-	if tag.selected then
-		item:get_children_by_id("tag")[1].markup = "<span foreground='"..beautiful.fg.."'>ur mom</span>"
-	elseif #tag:clients() > 0 then 
-		item:get_children_by_id("tag")[1].markup = "<span foreground='"..beautiful.fg.."'>has stuff</span>"
-	else
-		item:get_children_by_id("tag")[1].markup = "<span foreground='"..beautiful.fg.."'>empty</span>"
-	end
+    if tag.selected then
+        item:get_children_by_id("tag")[1].markup = "<span foreground='" .. beautiful.fg .. "'>ur mom</span>"
+    elseif #tag:clients() > 0 then
+        item:get_children_by_id("tag")[1].markup = "<span foreground='" .. beautiful.fg .. "'>has stuff</span>"
+    else
+        item:get_children_by_id("tag")[1].markup = "<span foreground='" .. beautiful.fg .. "'>empty</span>"
+    end
 end
 
 
@@ -65,8 +65,78 @@ screen.connect_signal("request::desktop_decoration", function(s)
     }
 
     -- Create a taglist widget
-    
 
+    s.mytaglist = awful.widget.taglist {
+        screen          = s,
+        filter          = awful.widget.taglist.filter.all,
+        style           = {
+            shape = gears.shape.powerline
+        },
+        layout          = {
+            spacing        = -12,
+            spacing_widget = {
+                color  = "#dddddd",
+                shape  = gears.shape.powerline,
+                widget = wibox.widget.separator,
+            },
+            layout         = wibox.layout.fixed.horizontal
+        },
+        widget_template = {
+            {
+                {
+                    {
+                        {
+                            {
+                                id     = "index_role",
+                                widget = wibox.widget.textbox,
+                            },
+                            margins = 4,
+                            widget  = wibox.container.margin,
+                        },
+                        bg     = "#dddddd",
+                        shape  = gears.shape.circle,
+                        widget = wibox.container.background,
+                    },
+                    {
+                        {
+                            id     = "icon_role",
+                            widget = wibox.widget.imagebox,
+                        },
+                        margins = 2,
+                        widget  = wibox.container.margin,
+                    },
+                    {
+                        id     = "text_role",
+                        widget = wibox.widget.textbox,
+                    },
+                    layout = wibox.layout.fixed.horizontal,
+                },
+                left   = 18,
+                right  = 18,
+                widget = wibox.container.margin
+            },
+            id              = "background_role",
+            widget          = wibox.container.background,
+            -- Add support for hover colors and an index label
+            create_callback = function(self, c3, index, objects) --luacheck: no unused args
+                self:get_children_by_id("index_role")[1].markup = "<b> " .. c3.index .. " </b>"
+                self:connect_signal("mouse::enter", function()
+                    if self.bg ~= "#ff0000" then
+                        self.backup     = self.bg
+                        self.has_backup = true
+                    end
+                    self.bg = "#ff0000"
+                end)
+                self:connect_signal("mouse::leave", function()
+                    if self.has_backup then self.bg = self.backup end
+                end)
+            end,
+            update_callback = function(self, c3, index, objects) --luacheck: no unused args
+                self:get_children_by_id("index_role")[1].markup = "<b> " .. c3.index .. " </b>"
+            end,
+        },
+        buttons         = taglist_buttons
+    }
 
     -- Create a tasklist widget
     s.mytasklist = awful.widget.tasklist {
@@ -91,7 +161,7 @@ screen.connect_signal("request::desktop_decoration", function(s)
         position = "top",
         screen   = s,
         bg       = "#0000",
-	margins = dpi(5),
+        margins  = dpi(5),
         widget   = {
             expand = "none",
             layout = wibox.layout.align.horizontal,
@@ -100,9 +170,9 @@ screen.connect_signal("request::desktop_decoration", function(s)
                 bg = "#ff0000",
                 shape = round_wibox_shape,
                 {
-			layout = wibox.layout.fixed.horizontal,
-			s.mytaglist,
-			s.mypromptbox,
+                    layout = wibox.layout.fixed.horizontal,
+                    s.mytaglist,
+                    s.mypromptbox,
                 }
             },
             {
@@ -110,8 +180,8 @@ screen.connect_signal("request::desktop_decoration", function(s)
                 bg = "#ff0000",
                 shape = round_wibox_shape,
                 {
-                        layout = wibox.layout.fixed.horizontal,
-                        s.mytasklist,
+                    layout = wibox.layout.fixed.horizontal,
+                    s.mytasklist,
                 }
             },
             {
@@ -119,11 +189,11 @@ screen.connect_signal("request::desktop_decoration", function(s)
                 bg = "#ff0000",
                 shape = round_wibox_shape,
                 {
-                        layout = wibox.layout.fixed.horizontal,
-                        mykeyboardlayout,
-                        wibox.widget.systray(),
-                        mytextclock,
-                        s.mylayoutbox,
+                    layout = wibox.layout.fixed.horizontal,
+                    mykeyboardlayout,
+                    wibox.widget.systray(),
+                    mytextclock,
+                    s.mylayoutbox,
                 },
             }
         }
