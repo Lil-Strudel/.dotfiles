b651975ade6317102036eaffbaea99b9420c7b9c
Lil Strudel <santoaaron@gmail.com>
2023-03-07 22:06:08 -0700

    Working diamond tag selector

--- .config/awesome/config/desktop_decoration.lua
+++ .config/awesome/config/desktop_decoration.lua
@@ -47,7 +47,7 @@ end
 
 screen.connect_signal("request::desktop_decoration", function(s)
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "1", "2", "3", "4", "5" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
@@ -70,71 +70,16 @@ screen.connect_signal("request::desktop_decoration", function(s)
         screen          = s,
         filter          = awful.widget.taglist.filter.all,
         style           = {
-            shape = gears.shape.powerline
-        },
-        layout          = {
-            spacing        = -12,
-            spacing_widget = {
-                color  = "#dddddd",
-                shape  = gears.shape.powerline,
-                widget = wibox.widget.separator,
-            },
-            layout         = wibox.layout.fixed.horizontal
-        },
-        widget_template = {
-            {
-                {
-                    {
-                        {
-                            {
-                                id     = "index_role",
-                                widget = wibox.widget.textbox,
-                            },
-                            margins = 4,
-                            widget  = wibox.container.margin,
-                        },
-                        bg     = "#dddddd",
-                        shape  = gears.shape.circle,
-                        widget = wibox.container.background,
-                    },
-                    {
-                        {
-                            id     = "icon_role",
-                            widget = wibox.widget.imagebox,
-                        },
-                        margins = 2,
-                        widget  = wibox.container.margin,
-                    },
-                    {
-                        id     = "text_role",
-                        widget = wibox.widget.textbox,
-                    },
-                    layout = wibox.layout.fixed.horizontal,
-                },
-                left   = 18,
-                right  = 18,
-                widget = wibox.container.margin
-            },
-            id              = "background_role",
-            widget          = wibox.container.background,
-            -- Add support for hover colors and an index label
-            create_callback = function(self, c3, index, objects) --luacheck: no unused args
-                self:get_children_by_id("index_role")[1].markup = "<b> " .. c3.index .. " </b>"
-                self:connect_signal("mouse::enter", function()
-                    if self.bg ~= "#ff0000" then
-                        self.backup     = self.bg
-                        self.has_backup = true
-                    end
-                    self.bg = "#ff0000"
-                end)
-                self:connect_signal("mouse::leave", function()
-                    if self.has_backup then self.bg = self.backup end
-                end)
-            end,
-            update_callback = function(self, c3, index, objects) --luacheck: no unused args
-                self:get_children_by_id("index_role")[1].markup = "<b> " .. c3.index .. " </b>"
-            end,
-        },
+    		spacing = dpi(15),
+		layout = wibox.layout.fixed.horizontal,
+		shape = gears.shape.losange,
+	},
+	widget_template = {
+		id = "background_role",
+		widget = wibox.container.background,
+		forced_width = dpi(15),
+		bg = "#00ff00"
+	},
         buttons         = taglist_buttons
     }
 
