d861c28740494f1922f6aa45ce9965cf13fa042a
Lil Strudel <santoaaron@gmail.com>
2023-03-19 20:59:11 -0600

    fix wallpaper

--- .config/awesome/config/wallpaper.lua
+++ .config/awesome/config/wallpaper.lua
@@ -3,17 +3,5 @@ local beautiful = require("beautiful")
 local wibox = require("wibox")
 
 screen.connect_signal("request::wallpaper", function(s)
-    awful.wallpaper {
-        screen = s,
-        widget =
-            {
-                image     = beautiful.wallpaper,
-                widget    = wibox.widget.imagebox,
-		horizontal_fit_policy = "auto",
-		vertical_fit_policy = "fit",
-		upscale = true,
-		downscale = true, 
-		resize = true, 
-            },
-    }
+    awful.spawn.with_shell("feh --bg-fill " .. beautiful.wallpaper)
 end)
