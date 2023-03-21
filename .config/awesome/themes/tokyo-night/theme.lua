c64191470d654f0664094a68db65937662855d97
Lil Strudel <santoaaron@gmail.com>
2023-03-20 22:03:32 -0600

    small changes

--- .config/awesome/themes/tokyo-night/theme.lua
+++ .config/awesome/themes/tokyo-night/theme.lua
@@ -136,8 +136,8 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
 theme.menu_submenu_icon = themes_path.."defaults/submenu.png"
-theme.menu_height = dpi(15)
-theme.menu_width  = dpi(100)
+theme.menu_height = dpi(20)
+theme.menu_width  = dpi(150)
 
 -- You can add as many variables as
 -- you wish and access them by using
