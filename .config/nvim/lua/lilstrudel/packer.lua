c27778a4e4be2d3542baca99362aea6e4cf94ef1
Lil Strudel <7326973-Lil-Strudel@users.noreply.gitlab.com>
2023-11-09 09:01:52 -0700

    Added noice

--- .config/nvim/lua/lilstrudel/packer.lua
+++ .config/nvim/lua/lilstrudel/packer.lua
@@ -66,6 +66,30 @@ return require('packer').startup(function(use)
         }
     }
 
+    use {
+        "folke/noice.nvim", 
+        config = function()
+            require("noice").setup({
+              lsp = {
+                override = {
+                  ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                  ["vim.lsp.util.stylize_markdown"] = true,
+                  ["cmp.entry.get_documentation"] = true,
+                },
+              },
+              presets = {
+                command_palette = true,
+                long_message_to_split = true, 
+                lsp_doc_border = true, 
+              },
+            })
+        end,
+        requires = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        }
+    }
+
     use 'christoomey/vim-tmux-navigator' 
 
     use({
