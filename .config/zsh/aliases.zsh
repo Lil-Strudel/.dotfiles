0cebfe28aca5afb992b56ed9a6c1402c621450fe
Aaron Santo <42657503+Lil-Strudel@users.noreply.github.com>
2023-12-06 16:34:47 -0700

    feat: terraform aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,3 +1,9 @@
 #!/bin/sh
 # Adding dotfiles bare repo for easy convinience
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+alias tf='terraform'
+alias tfin='terraform init'
+alias tfp='terraform plan'
+alias tfa='terraform apply'
+alias tff='terraform fmt'
