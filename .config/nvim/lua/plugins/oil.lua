f9a6fc3aca755b2bad8fbfc6ec6c526ffb963425
Lil Strudel <7326973-Lil-Strudel@users.noreply.gitlab.com>
2024-02-01 10:37:53 -0700

    fix: oil keybinds

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -6,7 +6,25 @@ return {
     { "<leader>pv", "<CMD>Oil<CR>" }
   },
   opts = {
-    -- use_default_keymaps = false,
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      -- ["<C-s>"] = "actions.select_vsplit",
+      -- ["<C-h>"] = "actions.select_split",
+      -- ["<C-t>"] = "actions.select_tab",
+      -- ["<C-p>"] = "actions.preview",
+      -- ["<C-c>"] = "actions.close",
+      -- ["<C-l>"] = "actions.refresh",
+      ["-"] = "actions.parent",
+      -- ["_"] = "actions.open_cwd",
+      -- ["`"] = "actions.cd",
+      -- ["~"] = "actions.tcd",
+      -- ["gs"] = "actions.change_sort",
+      -- ["gx"] = "actions.open_external",
+      -- ["g."] = "actions.toggle_hidden",
+      -- ["g\\"] = "actions.toggle_trash",
+    },
+    use_default_keymaps = false,
     view_options = {
       show_hidden = true
     }
