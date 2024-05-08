f13ee3a3ce5c31e85749deea82c795ca6992cc0f
Aaron Santo <42657503+Lil-Strudel@users.noreply.github.com>
2024-05-08 17:40:00 -0600

    fix: nvim dap

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -44,7 +44,10 @@ return {
     {
         "rcarriga/nvim-dap-ui",
         event = "VeryLazy",
-        dependencies = { "mfussenegger/nvim-dap" },
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio"
+        },
         config = function()
             local dap = require("dap")
             local dapui = require("dapui")
